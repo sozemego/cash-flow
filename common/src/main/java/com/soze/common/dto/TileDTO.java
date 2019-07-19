ffd33920aeec800ad0e77e8e93a16ba619cdf24d
sozemego <piknypiec@gmail.com>
2019-07-19 23:23:28 +0200

    working dtos

--- common/src/main/java/com/soze/common/dto/TileDTO.java
+++ common/src/main/java/com/soze/common/dto/TileDTO.java
@@ -2,4 +2,27 @@ package com.soze.common.dto;
 
 public class TileDTO {
 
+  private int x;
+  private int y;
+
+  public TileDTO(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
 }
--- common/src/main/java/com/soze/common/dto/WorldDTO.java
+++ common/src/main/java/com/soze/common/dto/WorldDTO.java
@@ -5,18 +5,33 @@ import java.util.List;
 public class WorldDTO {
 
   private final List<TileDTO> tiles;
-  private final int tileSize;
+  private final int tileWidth;
+  private final int tileHeight;
 
-  public WorldDTO(List<TileDTO> tiles, int tileSize) {
+  public WorldDTO(List<TileDTO> tiles, int tileWidth, int tileHeight) {
     this.tiles = tiles;
-    this.tileSize = tileSize;
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
   }
 
   public List<TileDTO> getTiles() {
     return tiles;
   }
 
-  public int getTileSize() {
-    return tileSize;
+  public int getTileWidth() {
+    return tileWidth;
+  }
+
+  public int getTileHeight() {
+    return tileHeight;
+  }
+
+  @Override
+  public String toString() {
+    return "WorldDTO{" +
+        "tiles count=" + tiles.size() +
+        ", tileWidth=" + tileWidth +
+        ", tileHeight=" + tileHeight +
+        '}';
   }
 }
