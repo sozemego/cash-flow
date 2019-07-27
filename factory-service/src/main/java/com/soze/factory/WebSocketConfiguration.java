b4df969b3ee3d640d4790f1adf11f33032701db8
sozemego <piknypiec@gmail.com>
2019-07-28 00:27:32 +0200

    Factory service websocket does not use stomp

--- factory-service/src/main/java/com/soze/factory/WebSocketConfiguration.java
+++ factory-service/src/main/java/com/soze/factory/WebSocketConfiguration.java
@@ -1,23 +1,26 @@
 package com.soze.factory;
 
+import com.soze.factory.controller.FactoryWebSocketController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/topic");
-    registry.setApplicationDestinationPrefixes("/factory");
+  private final FactoryWebSocketController factoryWebSocketController;
+
+  @Autowired
+  public WebSocketConfiguration(
+      FactoryWebSocketController factoryWebSocketController) {
+    this.factoryWebSocketController = factoryWebSocketController;
   }
 
   @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/factory-ws").withSockJS();
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+    webSocketHandlerRegistry.addHandler(factoryWebSocketController, "/websocket");
   }
 }
--- factory-service/src/main/java/com/soze/factory/controller/FactoryWebSocketController.java
+++ factory-service/src/main/java/com/soze/factory/controller/FactoryWebSocketController.java
@@ -1,16 +1,28 @@
 package com.soze.factory.controller;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import com.soze.factory.service.FactoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@Controller
-public class FactoryWebSocketController {
+@Component
+public class FactoryWebSocketController extends TextWebSocketHandler {
 
-  @MessageMapping("/init")
-  @SendTo("/topic/init-response")
-  public String handleInit() {
-    return "YES IM HERE";
+  private static final Logger LOG = LoggerFactory.getLogger(FactoryWebSocketController.class);
+
+  private final FactoryService factoryService;
+
+  @Autowired
+  public FactoryWebSocketController(FactoryService factoryService) {
+    this.factoryService = factoryService;
   }
 
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    LOG.info("{} connected", session.getId());
+    super.afterConnectionEstablished(session);
+  }
 }
