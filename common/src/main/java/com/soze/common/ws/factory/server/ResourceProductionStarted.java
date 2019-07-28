d1e525ec6c445954cd369a542d54c22e9a0879d8
sozemego <piknypiec@gmail.com>
2019-07-28 19:33:46 +0200

    ResourceProductionStarted - new ServerMessage type

---
+++ common/src/main/java/com/soze/common/ws/factory/server/ResourceProductionStarted.java
@@ -0,0 +1,38 @@
+package com.soze.common.ws.factory.server;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.soze.common.dto.Resource;
+import java.util.UUID;
+
+public class ResourceProductionStarted extends ServerMessage {
+
+  private final String factoryId;
+  private final Resource resource;
+
+  @JsonCreator
+  public ResourceProductionStarted(@JsonProperty("messageId") UUID messageId,
+                                   @JsonProperty("factoryId") String factoryId,
+                                   @JsonProperty("resource") Resource resource) {
+    super(messageId);
+    this.factoryId = factoryId;
+    this.resource = resource;
+  }
+
+  public ResourceProductionStarted(String factoryId, Resource resource) {
+    this(UUID.randomUUID(), factoryId, resource);
+  }
+
+  public String getFactoryId() {
+    return factoryId;
+  }
+
+  public Resource getResource() {
+    return resource;
+  }
+
+  @Override
+  public String getType() {
+    return ServerMessageType.RESOURCE_PRODUCTION_STARTED.name();
+  }
+}
--- common/src/main/java/com/soze/common/ws/factory/server/ServerMessage.java
+++ common/src/main/java/com/soze/common/ws/factory/server/ServerMessage.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 )
 @JsonSubTypes(value = {
     @JsonSubTypes.Type(value = ResourceProduced.class, name = "RESOURCE_PRODUCED"),
+    @JsonSubTypes.Type(value = ResourceProductionStarted.class, name = "RESOURCE_PRODUCTION_STARTED"),
     @JsonSubTypes.Type(value = FactoryAdded.class, name = "FACTORY_ADDED"),
 })
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -32,6 +33,6 @@ public abstract class ServerMessage {
   public abstract String getType();
 
   public enum ServerMessageType {
-    RESOURCE_PRODUCED, FACTORY_ADDED
+    RESOURCE_PRODUCED, RESOURCE_PRODUCTION_STARTED, FACTORY_ADDED
   }
 }
