9a797cc34999b40de7b0fad9ca1e10645f3725fa
sozemego <piknypiec@gmail.com>
2019-08-14 17:46:26 +0200

    Module for warehouse service

---
+++ warehouse-service/src/main/java/com/soze/warehouse/WarehouseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.soze.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseServiceApplication.class, args);
+	}
+
+}
