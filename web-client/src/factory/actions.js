570e9e4378e25bf719be85e7f911bed0071f3eff
sozemego <piknypiec@gmail.com>
2019-08-15 21:09:49 +0200

    Handle RESOURCE_PRODUCED server message

--- web-client/src/factory/actions.js
+++ web-client/src/factory/actions.js
@@ -1,3 +1,3 @@
-import { makeActionCreator } from "../store/actionCreator";
-
 export const FACTORY_ADDED = "FACTORY_ADDED";
+export const RESOURCE_PRODUCED = "RESOURCE_PRODUCED";
+export const RESOURCE_PRODUCTION_STARTEd = "RESOURCE_PRODUCTION_STARTED";
--- web-client/src/factory/reducer.js
+++ web-client/src/factory/reducer.js
@@ -1,4 +1,4 @@
-import { FACTORY_ADDED } from "./actions";
+import { FACTORY_ADDED, RESOURCE_PRODUCED } from "./actions";
 
 const initialState = {
   factories: []
@@ -8,6 +8,8 @@ export function reducer(state = initialState, action) {
   switch (action.type) {
     case FACTORY_ADDED:
       return factoryAdded(state, action);
+    case RESOURCE_PRODUCED:
+      return resourceProduced(state, action);
     default:
       return state;
   }
@@ -18,4 +20,22 @@ function factoryAdded(state, action) {
   const { factoryDTO } = action;
 
   return { ...state, factories: [...previousFactories, factoryDTO] };
+}
+
+function resourceProduced(state, action) {
+  const { factoryId, resource } = action;
+  const factories = [...state.factories];
+  const index = factories.findIndex(factory => factory.id == factoryId);
+  if (index > -1) {
+    const factory = { ...factories[index] };
+    const storage = { ...factory.storage };
+    const resources = { ...storage.resources };
+    const count = resources[resource] || 0;
+    storage.resources = resources;
+    factory.storage = storage;
+    resources[resource] = count + 1;
+    factories[index] = factory;
+  }
+
+  return { ...state, factories };
 }
