303ebd92e4a994c6f9c0e71564a82ba56284f02f
sozemego <piknypiec@gmail.com>
2019-08-23 22:49:17 +0200

    CORS config for WorldService

---
+++ world-service/src/main/java/com/soze/world/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.soze.world;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+						.addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+		registry.addResourceHandler("/webjars/**")
+						.addResourceLocations("classpath:/META-INF/resources/webjars/");
+		super.addResourceHandlers(registry);
+	}
+}
