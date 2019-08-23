2fdbbcf37ae7205ac1074dd246448fb160c88f1e
sozemego <piknypiec@gmail.com>
2019-08-24 01:02:27 +0200

    Exposed two endpoints /id/{cityId} and /name/{name} for getting cities by name or id

--- world-service/src/main/java/com/soze/world/controller/WorldController.java
+++ world-service/src/main/java/com/soze/world/controller/WorldController.java
@@ -8,9 +8,12 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -35,6 +38,28 @@ public class WorldController {
 		List<City> cities = worldService.getCities();
 		LOG.info("Returning {} cities", cities.size());
 		return cityConverter.convert(cities);
+	}
+
+	@GetMapping(value = "/id/{cityId}")
+	public CityDTO getCityById(@PathVariable("cityId") String cityId, HttpServletResponse response) {
+		LOG.info("Called getCityById with id = {}", cityId);
+		return worldService.getCityById(cityId)
+											 .map(cityConverter::convert)
+											 .orElseGet(() -> {
+												 response.setStatus(HttpStatus.NOT_FOUND.value());
+												 return null;
+											 });
+	}
+
+	@GetMapping(value = "/name/{name}")
+	public CityDTO getCityByName(@PathVariable("name") String name, HttpServletResponse response) {
+		LOG.info("Called getCityByName with name = {}", name);
+		return worldService.getCityByName(name)
+											 .map(cityConverter::convert)
+											 .orElseGet(() -> {
+												 response.setStatus(HttpStatus.NOT_FOUND.value());
+												 return null;
+											 });
 	}
 
 }
