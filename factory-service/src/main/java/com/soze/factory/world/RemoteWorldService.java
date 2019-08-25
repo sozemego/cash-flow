e6f23cb5baeb93cf5d9bef92389ef4bac2f7748c
sozemego <piknypiec@gmail.com>
2019-08-25 18:56:05 +0200

    RemoteWorldService now retries its calls

--- factory-service/src/main/java/com/soze/factory/world/RemoteWorldService.java
+++ factory-service/src/main/java/com/soze/factory/world/RemoteWorldService.java
@@ -2,11 +2,13 @@ package com.soze.factory.world;
 
 import com.soze.common.client.WorldServiceClient;
 import com.soze.common.dto.CityDTO;
+import com.soze.common.resilience.RetryUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -22,14 +24,14 @@ public class RemoteWorldService {
 	}
 
 	public List<CityDTO> getAllCities() {
-		return client.getAllCities();
+		return RetryUtils.retry(25, Duration.ofMillis(500), client::getAllCities);
 	}
 
 	public CityDTO getCityByName(String name) {
-		return client.getCityByName(name);
+		return RetryUtils.retry(25, Duration.ofMillis(500), () -> client.getCityByName(name));
 	}
 
 	public CityDTO getCityById(String cityId) {
-		return client.getCityById(cityId);
+		return RetryUtils.retry(25, Duration.ofMillis(500), () -> client.getCityById(cityId));
 	}
 }
