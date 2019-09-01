672ada92d7c7a9e65bdcf45217e3102b73acf2de
sozemego <piknypiec@gmail.com>
2019-09-01 20:05:10 +0200

    Remove console.log

--- web-client/src/clock/Clock.js
+++ web-client/src/clock/Clock.js
@@ -6,7 +6,6 @@ export function Clock() {
 	const { clock } = useGameClock({interval: 1000});
 
 	const date = getCurrentGameDate(clock);
-	console.log('current game time from Clock.js', date);
 
   return <div>{getFormattedDateTime(date)}</div>;
 }
