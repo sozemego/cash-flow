cefbfe674adb2b466c9a73821dc8c71a80abe873
sozemego <piknypiec@gmail.com>
2019-09-05 21:24:55 +0200

    clock-service now uses eureka service discovery and exposes a Feign client to other consumers

--- clock-service/src/main/java/com/soze/clock/client/ClockServiceClient.java
+++ clock-service/src/main/java/com/soze/clock/client/ClockServiceClient.java
@@ -1,26 +1,13 @@
 package com.soze.clock.client;
 
 import com.soze.clock.domain.Clock;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
+@FeignClient("clock-service")
 public interface ClockServiceClient {
 
-	String CLOCK_SERVICE_URL = "http://localhost:9004/clock/";
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Clock getClock();
-
-	static ClockServiceClient createClient() {
-		ResteasyClient client = (ResteasyClient) ResteasyClientBuilder.newClient();
-		ResteasyWebTarget target = client.target(CLOCK_SERVICE_URL);
-		return target.proxy(ClockServiceClient.class);
-	}
-
+	@GetMapping(value = "/clock", produces = MediaType.APPLICATION_JSON_VALUE)
+	Clock getClock();
 }
--- clock-service/src/main/java/com/soze/clock/controller/ClockController.java
+++ clock-service/src/main/java/com/soze/clock/controller/ClockController.java
@@ -1,15 +1,15 @@
 package com.soze.clock.controller;
 
+import com.soze.clock.client.ClockServiceClient;
 import com.soze.clock.domain.Clock;
 import com.soze.clock.service.ClockService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClockController {
+public class ClockController implements ClockServiceClient {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ClockController.class);
 
@@ -20,7 +20,6 @@ public class ClockController {
 		this.clockService = clockService;
 	}
 
-	@GetMapping(value = "/")
 	public Clock getClock() {
 		LOG.info("Called getClock");
 		Clock clock = new Clock(
