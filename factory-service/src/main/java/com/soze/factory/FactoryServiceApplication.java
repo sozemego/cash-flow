8386f83b9453b2661e014036f21f2e0b4274ec8f
sozemego <piknypiec@gmail.com>
2019-09-05 21:37:51 +0200

    factory-service registers as a service and uses registered clock-service from service-discovery

--- factory-service/src/main/java/com/soze/factory/FactoryServiceApplication.java
+++ factory-service/src/main/java/com/soze/factory/FactoryServiceApplication.java
@@ -6,8 +6,11 @@ import com.soze.common.client.WorldServiceClient;
 import com.soze.common.resilience.RetryUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,6 +19,8 @@ import java.time.Duration;
 
 @SpringBootApplication
 @Configuration
+@EnableDiscoveryClient
+@EnableFeignClients("com.soze.clock.client")
 public class FactoryServiceApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(FactoryServiceApplication.class);
@@ -29,14 +34,16 @@ public class FactoryServiceApplication {
 		return WorldServiceClient.createClient();
 	}
 
+	@Autowired
+	private ClockServiceClient clockServiceClient;
+
 	@Bean
 	@Profile("!test")
 	public Clock clock() {
 		LOG.info("Init clock ...");
-		ClockServiceClient clockClient = ClockServiceClient.createClient();
 		return RetryUtils.retry(25, Duration.ofMillis(2000), () -> {
 			try {
-				return clockClient.getClock();
+				return clockServiceClient.getClock();
 			} catch (Exception e) {
 				LOG.info("Error during clock fetching", e);
 				throw e;
