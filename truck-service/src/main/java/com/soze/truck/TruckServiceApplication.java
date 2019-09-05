f2272b3e9f1bffa9b6b788a35eb22964ab880eb3
sozemego <piknypiec@gmail.com>
2019-09-05 22:10:56 +0200

    truck-service now registers as a service in eureka server and uses clock-service FeignClient

--- truck-service/src/main/java/com/soze/truck/TruckServiceApplication.java
+++ truck-service/src/main/java/com/soze/truck/TruckServiceApplication.java
@@ -6,8 +6,10 @@ import com.soze.common.resilience.RetryUtils;
 import com.soze.factory.client.FactoryServiceClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
@@ -15,6 +17,7 @@ import java.time.Duration;
 
 
 @SpringBootApplication
+@EnableFeignClients("com.soze.clock.client")
 public class TruckServiceApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TruckServiceApplication.class);
@@ -23,10 +26,12 @@ public class TruckServiceApplication {
 		SpringApplication.run(TruckServiceApplication.class, args);
 	}
 
+	@Autowired
+	private ClockServiceClient clockServiceClient;
+
 	@Bean
 	@Profile("!test")
 	public Clock clock() {
-		ClockServiceClient clockServiceClient = ClockServiceClient.createClient();
 		return RetryUtils.retry(25, Duration.ofMillis(2500), () -> {
 			LOG.info("Fetching clock");
 			try {
