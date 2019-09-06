f4933c3ac93ecfe4ecdc64d91be8d762ed9c4ac3
sozemego <piknypiec@gmail.com>
2019-09-06 21:36:28 +0200

    Change WorldServiceClient into a FeignClient

--- common/src/main/java/com/soze/common/client/WorldServiceClient.java
+++ common/src/main/java/com/soze/common/client/WorldServiceClient.java
@@ -1,35 +1,32 @@
 package com.soze.common.client;
 
 import com.soze.common.dto.CityDTO;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@FeignClient("world-service")
 public interface WorldServiceClient {
 
-	String WORLD_SERVICE_URL = "http://localhost:9000/";
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@GetMapping(
+		path = "/world",
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
 	List<CityDTO> getAllCities();
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/name/{name}")
-	CityDTO getCityByName(@PathParam("name") String name);
+	@GetMapping(
+		path = "/world/name/{name}",
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	CityDTO getCityByName(@PathVariable("name") String name);
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/id/{cityId}")
-	CityDTO getCityById(@PathParam("cityId") String cityId);
+	@GetMapping(
+		path = "/world/id/{cityId}",
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	CityDTO getCityById(@PathVariable("cityId") String cityId);
 
-	static WorldServiceClient createClient() {
-		ResteasyClient client = (ResteasyClient) ResteasyClientBuilder.newClient();
-		ResteasyWebTarget target = client.target(WORLD_SERVICE_URL);
-		return target.proxy(WorldServiceClient.class);
-	}
 }
