d6e2a2f8c957a2bb246f62d801e68b742a87a587
sozemego <piknypiec@gmail.com>
2019-09-08 18:21:55 +0200

    change websocket urls to account for recently removed context-paths from various services

--- web-client/src/config/urls.js
+++ web-client/src/config/urls.js
@@ -1,3 +1,3 @@
-export const WORLD_SERVICE_URL = "http://localhost:9000/world";
-export const FACTORY_SERVICE_URL = "ws://localhost:9001/factory";
-export const TRUCK_SERVICE_URL = "ws://localhost:9003/truck";
+export const WORLD_SERVICE_URL = "http://localhost:9000/";
+export const FACTORY_SERVICE_URL = "ws://localhost:9001/";
+export const TRUCK_SERVICE_URL = "ws://localhost:9003/";
