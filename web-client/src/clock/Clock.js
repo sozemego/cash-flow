0a8064c9461b1d13bb8cb765d173f1fb88445e53
sozemego <piknypiec@gmail.com>
2019-09-29 18:18:21 +0200

    Refactor style of Clock

--- web-client/src/clock/Clock.js
+++ web-client/src/clock/Clock.js
@@ -1,30 +1,12 @@
 import React from "react";
-import { DatePicker, TimePicker } from "antd";
-import moment from "moment";
 
 import { useGameClock } from "./gameClock";
+import Text from "antd/lib/typography/Text";
+import { getFormattedDateTime } from "./business";
 
-const dateFormat = `DD-MM-YYYY`;
-const timeFormat = "HH:mm";
 
 export function Clock() {
   const { date } = useGameClock({ interval: 1000 });
 
-  return (
-    <>
-      <DatePicker
-        value={moment(
-          `${date.getDate() + 1}/${date.getMonth()}/${date.getFullYear()}`,
-          dateFormat
-        )}
-        format={dateFormat}
-				allowClear={false}
-      />
-      <TimePicker
-        value={moment(`${date.getHours()}:${date.getMinutes()}`, timeFormat)}
-        format={timeFormat}
-				allowClear={false}
-      />
-    </>
-  );
+  return <Text code>{getFormattedDateTime(date)}</Text>;
 }
