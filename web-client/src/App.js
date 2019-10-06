9ff8e3b2a34b74c7cf7841aac0d42e823bebee5c
sozemego <piknypiec@gmail.com>
2019-10-06 21:35:18 +0200

    Gray background for game header

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -7,12 +7,15 @@ import 'antd/dist/antd.css';
 function App() {
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>Cash flow</h2>
-      <div style={{ textAlign: "center" }}>
-        <Clock />
-        <Player />
-        <hr />
+      <div style={{backgroundColor: "#e7e7e7"}}>
+        <h2 style={{ textAlign: "center" }}>Cash flow</h2>
+        <div style={{ textAlign: "center" }}>
+          <Clock />
+          <Player />
+          <hr />
+        </div>
       </div>
+
       <Game />
     </div>
   );
