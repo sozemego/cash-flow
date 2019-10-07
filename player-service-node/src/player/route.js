f6e6c0245eb99c3d57ea18a855b9422e1a38086f
sozemego <piknypiec@gmail.com>
2019-10-07 21:06:20 +0200

    Working /transfer endpoint for new node.js player-service

--- player-service-node/src/player/route.js
+++ player-service-node/src/player/route.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const { getPlayer } = require("./service");
+const { getPlayer, transfer } = require("./service");
 
 router.use((req, res, next) => {
-  console.log("Request at " + new Date().toISOString() + " to get player");
+  console.log("Request at " + new Date().toISOString() + " to " + req.path + " " + JSON.stringify(req.query));
   next();
 });
 
 router.get("/player", (req, res) => {
   res.send(getPlayer());
+});
+
+router.post("/transfer", (req, res) => {
+  const { query } = req;
+  const amount = Number(query.amount);
+  console.log(`Call to transfer with ${amount}`);
+  if (!amount) {
+    res.status(400).end();
+    return;
+  }
+
+  const result = transfer(amount);
+  console.log(result);
+  res.send(result);
 });
 
 module.exports = router;
--- player-service-node/src/player/service.js
+++ player-service-node/src/player/service.js
@@ -1,8 +1,33 @@
-const player = {
-  cash: 500,
-	name: 'Great owner'
-};
+const fs = require("fs");
+
+const playerString = fs.readFileSync("player.json", { encoding: "utf-8" });
+const player = JSON.parse(playerString);
+
+function transfer(amount) {
+  console.log(`Attempting to transfer ${amount} to player ${JSON.stringify(player)}`);
+
+  if (amount > 0) {
+  	player.cash += amount;
+  	savePlayer();
+    return { amountTransferred: amount, current: player.cash };
+  }
+
+  const { cash } = player;
+  if (cash < Math.abs(amount)) {
+    return { amountTransferred: 0, current: player.cash };
+  }
+
+  player.cash += amount;
+  savePlayer()
+  return { amountTransferred: amount, current: player.cash };
+}
+
+function savePlayer() {
+	console.log('Saving player');
+	fs.writeFileSync('player.json', JSON.stringify(player));
+}
 
 module.exports = {
-  getPlayer: () => player
+  getPlayer: () => player,
+	transfer
 };
