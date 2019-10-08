664951a1993fbcdede935eab6f1920a5b8aff440
sozemego <piknypiec@gmail.com>
2019-10-08 08:54:32 +0200

    Order switch of URLs

--- web-client/src/config/urls.js
+++ web-client/src/config/urls.js
@@ -3,5 +3,5 @@ export const FACTORY_SERVICE_URL = "http://localhost:9001";
 export const FACTORY_SERVICE_URL_EVENTS = `${FACTORY_SERVICE_URL}/events`;
 export const FACTORY_SERVICE_SOCKET_URL = "ws://localhost:9001/websocket";
 export const TRUCK_SERVICE_URL = "ws://localhost:9003";
-export const PLAYER_SERVICE_SOCKET_URL = "ws://localhost:9005/websocket";
 export const PLAYER_SERVICE_URL = "http://localhost:9005";
+export const PLAYER_SERVICE_SOCKET_URL = "ws://localhost:9005/websocket";
