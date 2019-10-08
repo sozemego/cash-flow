8ce3c044248c268289e06e363111e4dd48903b32
sozemego <piknypiec@gmail.com>
2019-10-08 17:11:30 +0200

    player-service-node now registers itself as an eureka client to eureka server

--- player-service-node/src/index.js
+++ player-service-node/src/index.js
@@ -1,6 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+const { Eureka } = require("eureka-js-client");
 
 const playerRouter = require("./player/route");
 const { startWebsocket } = require("./player/ws");
@@ -18,8 +22,45 @@ app.use("/", playerRouter);
 
 startWebsocket(server);
 
-server.listen(port, () => console.log("App listening on " + port));
+let eurekaClient = null;
+
+server.listen(port, () => {
+  console.log("App listening on " + port);
+
+  console.log("Registering in eureka service discovery");
+  eurekaClient = new Eureka({
+    // application instance information
+    instance: {
+      id: "player-service",
+      instanceId: "player-service",
+      app: "PLAYER-SERVICE",
+      hostName: "localhost",
+      ipAddr: "127.0.0.1",
+      statusPageUrl: 'http://localhost:8080/info',
+      port: {
+        '$': 9005,
+        '@enabled': true,
+      },
+      vipAddress: "player-service",
+      dataCenterInfo: {
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+        name: 'MyOwn',
+      },
+    },
+    eureka: {
+      // eureka server host / port
+      host: "localhost",
+      port: 8761,
+      servicePath: "/eureka/apps/"
+    }
+  });
+  eurekaClient.start();
+});
 
 module.exports = {
   server
 };
+
+process.on("exit", code => {
+  console.log(`About to exit with code: ${code}`);
+});
