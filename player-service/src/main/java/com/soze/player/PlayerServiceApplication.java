5b53335f961a2afa022540d8465f5d4cd3bffd8d
sozemego <piknypiec@gmail.com>
2019-10-12 10:20:00 +0200

    player-service - deprecation

--- player-service/src/main/java/com/soze/player/PlayerServiceApplication.java
+++ player-service/src/main/java/com/soze/player/PlayerServiceApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * This app is deprecated. See player-service-node.
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
+@Deprecated
 public class PlayerServiceApplication {
 
 	public static void main(String[] args) {
