0719cb104da2d2cbfffe358404ca8a9ee0ee50ab
sozemego <piknypiec@gmail.com>
2019-10-16 20:31:11 +0200

    web-client - show factory storage

--- web-client/src/factory/FactoryStorage.tsx
+++ web-client/src/factory/FactoryStorage.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { IFactoryStorage } from "./index.d";
+import {ResourceIcon} from "../components/ResourceIcon";
 
 export interface FactoryStorageProps {
   storage: IFactoryStorage;
 }
 
 export function FactoryStorage({ storage }: FactoryStorageProps) {
-  return <div>THIS IS FACTORY STORAGE</div>;
+  const { capacities, resources } = storage;
+
+  return (
+    <>
+      {Object.entries(capacities).map(([resource, capacity]) => {
+          const capacityTaken = resources[resource] || 0;
+          return (
+              <div>
+                  <ResourceIcon resource={resource}/>
+                  {capacityTaken} / {capacity}
+              </div>
+          )
+      })}
+    </>
+  );
 }
 
 /**
@@ -63,15 +78,15 @@ export function addResource(storage, resource, count = 1) {
  * @param storages
  */
 export function combine(storages: IFactoryStorage[]): IFactoryStorage {
-    const totalStorage = { capacities: {}, resources: {} };
-    for (let storage of storages) {
-        const totalCapacities = totalStorage.capacities;
-        const capacities = storage.capacities;
-        Object.entries(capacities).forEach(([resource, capacity]) => {
-            const totalCapacity = totalCapacities[resource] || 0;
-            totalCapacities[resource] = totalCapacity + (capacity as number);
-        });
-        transfer(storage, totalStorage);
-    }
-    return totalStorage;
+  const totalStorage = { capacities: {}, resources: {} };
+  for (let storage of storages) {
+    const totalCapacities = totalStorage.capacities;
+    const capacities = storage.capacities;
+    Object.entries(capacities).forEach(([resource, capacity]) => {
+      const totalCapacity = totalCapacities[resource] || 0;
+      totalCapacities[resource] = totalCapacity + (capacity as number);
+    });
+    transfer(storage, totalStorage);
+  }
+  return totalStorage;
 }
