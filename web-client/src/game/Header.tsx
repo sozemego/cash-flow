678499647ee8ed9e977308ab74d01a59f60c7c5f
sozemego <piknypiec@gmail.com>
2019-10-19 14:42:36 +0200

    web-client - show average price of resource and its saturation in the factories

--- web-client/src/game/Header.tsx
+++ web-client/src/game/Header.tsx
@@ -7,7 +7,9 @@ import Modal from "antd/lib/modal";
 import Table from "antd/lib/table";
 import { ResourceIcon } from "../components/ResourceIcon";
 import { useGetFactories } from "../factory/selectors";
-import {IStorageSlot} from "../factory/index.d";
+import { IFactory, IStorageSlot, IStorageSlotEntry } from "../factory/index.d";
+import {Resource} from "../world/index.d";
+import {ResourceMap} from "../world/reducer";
 
 export function Header(props) {
   return (
@@ -33,23 +35,52 @@ export function Header(props) {
   );
 }
 
-function calcTotalResourceCounts(factories) {
-  const counts = {};
+interface ResourceDatas {
+  [key: string]: ResourceData;
+}
+
+interface ResourceData {
+  count: number;
+  averagePrice: number;
+  saturation: number;
+  factories: number;
+}
+
+function calcTotalResources(factories: IFactory[], resources: ResourceMap): ResourceDatas {
+  const datas: ResourceDatas = {};
   factories.forEach(factory => {
     const { storage } = factory;
-    Object.entries(storage).forEach(([resource, slot]) => {
-      const actualCount = counts[resource] || 0;
-      counts[resource] = actualCount + (slot as IStorageSlot).count;
-    })
+    Object.entries(storage).forEach(([resource, slot]: IStorageSlotEntry) => {
+      const defaultData: ResourceData = {
+        count: 0,
+        averagePrice: 0,
+        saturation: 0,
+        factories: 0
+      };
+      const data = datas[resource] || defaultData;
+      data.count += slot.count;
+      data.averagePrice += slot.price;
+      data.saturation += slot.count / slot.capacity;
+      data.factories += 1;
+      datas[resource] = data;
+    });
   });
-  return counts;
+  Object.keys(resources).forEach(resource => {
+    const data = datas[resource];
+    if (!data) {
+      return;
+    }
+    data.averagePrice = data.averagePrice / data.factories;
+    data.saturation = data.saturation / data.factories;
+  });
+  return datas;
 }
 
 function Resources() {
   const [show, setShow] = useState(false);
   const resources = useGetResources();
   const factories = useGetFactories();
-  const resourceCounts = calcTotalResourceCounts(factories);
+  const totalResources = calcTotalResources(factories, resources);
 
   function close() {
     setShow(false);
@@ -71,14 +102,30 @@ function Resources() {
       }
     },
     { title: "Price", dataIndex: "price", key: "price" },
-    { title: "In factories", dataIndex: "countInFactories", key: "countInFactories" }
+    {
+      title: "In factories",
+      dataIndex: "countInFactories",
+      key: "countInFactories"
+    },
+    {
+      title: "Average price / saturation",
+      dataIndex: "priceSaturation",
+      key: "priceSaturation"
+    }
   ];
   const data = Object.values(resources).map(resource => {
+    const resourceName = resource.name.toUpperCase();
+    const data = totalResources[resourceName] || {
+      count: 0,
+      saturation: 0,
+      averagePrice: 0
+    };
     return {
       key: resource.name,
       name: resource.name,
       price: `${resource.minPrice} - ${resource.maxPrice}`,
-      countInFactories: resourceCounts[resource.name.toUpperCase()]
+      countInFactories: data.count,
+      priceSaturation: `${data.averagePrice.toFixed(1)} (${(data.saturation * 100).toFixed(0)}%)`
     };
   });
 
