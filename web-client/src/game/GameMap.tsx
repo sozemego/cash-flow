70ebd94cdfd3fd41cc575c19b0f7c51a73ba229d
sozemego <piknypiec@gmail.com>
2019-10-30 20:35:35 +0100

    web-client - show lines between current truck position and target position on GameMap

--- web-client/src/game/GameMap.tsx
+++ web-client/src/game/GameMap.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Icon } from "antd";
 import Modal from "antd/lib/modal";
-import { Marker, TileLayer, Map, Tooltip } from "react-leaflet";
+import { Marker, TileLayer, Map, Tooltip, Polyline } from "react-leaflet";
 import Leaflet from "leaflet";
 import { LatLngTuple } from "leaflet";
 import { useGetCities } from "../world/selectors";
 import { ICity } from "../world";
 import { useGetTrucks } from "../truck/selectors";
 import { ITruck } from "../truck";
+import { useGameClock } from "../clock/useGameClock";
 
 function randomDeviation(position: LatLngTuple): LatLngTuple {
   const deviation = 0.005;
@@ -27,15 +28,23 @@ export function GameMap() {
 
   const cities: ICity[] = Object.values(useGetCities());
   const trucks: ITruck[] = Object.values(useGetTrucks());
+  const trucksAtCity = trucks.filter(
+    truck => truck.navigation.nextCityId === null
+  );
+  const travellingTrucks = trucks.filter(
+    truck => truck.navigation.nextCityId !== null
+  );
 
   function getPosition(truck: ITruck): LatLngTuple {
-    const city = cities.find(
-      city => city.id === truck.navigation.currentCityId
-    );
+    return randomDeviation(getCityPosition(truck.navigation.currentCityId));
+  }
+
+  function getCityPosition(cityId: string): LatLngTuple {
+    const city = cities.find(city => city.id === cityId);
     if (!city) {
       return [0, 0];
     }
-    return randomDeviation([city.latitude, city.longitude]);
+    return [city.latitude, city.longitude];
   }
 
   function getTruckIcon(truck: ITruck): Leaflet.Icon {
@@ -50,6 +59,13 @@ export function GameMap() {
       iconSize: [24, 24]
     });
   }
+
+  function getCityName(cityId: string): string {
+    const city = cities.find(city => city.id === cityId);
+    return city ? city.name : '';
+  }
+
+  const { time } = useGameClock({ interval: 1000 });
 
   return (
     <Map
@@ -66,19 +82,52 @@ export function GameMap() {
           <Tooltip>{city.name}</Tooltip>
         </Marker>
       ))}
-      {trucks.map(truck => (
+      {trucksAtCity.map(truck => (
         <Marker
           key={truck.id}
           position={getPosition(truck)}
           icon={getTruckIcon(truck)}
         />
       ))}
+      {travellingTrucks.map(truck => {
+        const { navigation } = truck;
+        const {
+          nextCityId,
+          currentCityId,
+          startTime,
+          arrivalTime
+        } = navigation;
+        const startCity = getCityPosition(currentCityId);
+        const endCity = getCityPosition(nextCityId!);
+        let travelTimePassed = time - startTime;
+        const totalTime = arrivalTime - startTime;
+        const percentTravelled = travelTimePassed / totalTime;
+
+        const latDifference = endCity[0] - startCity[0];
+        const longDifference = endCity[1] - startCity[1];
+
+        const start: LatLngTuple = [
+          startCity[0] + percentTravelled * latDifference,
+          startCity[1] + percentTravelled * longDifference
+        ];
+
+        const positions = [start, endCity];
+
+        return (
+          <>
+            <Polyline positions={positions} />
+            <Marker key={truck.id} position={start} icon={getTruckIcon(truck)}>
+              <Tooltip>{truck.name} travelling to {getCityName(nextCityId!)}</Tooltip>
+            </Marker>
+          </>
+        );
+      })}
     </Map>
   );
 }
 
 export function GameMapIcon() {
-  const [showMap, setShowMap] = React.useState<boolean>(true);
+  const [showMap, setShowMap] = React.useState<boolean>(false);
 
   return (
     <>
