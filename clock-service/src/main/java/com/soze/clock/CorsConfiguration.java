a89f3f5c615d71dc8dd08112214995493bb724de
sozemego <piknypiec@gmail.com>
2019-11-11 21:22:41 +0100

    clock-service - disable CORS

--- clock-service/src/main/java/com/soze/clock/CorsConfiguration.java
+++ clock-service/src/main/java/com/soze/clock/CorsConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
