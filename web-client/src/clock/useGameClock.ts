1c327ca5562fff32292e53952930c477b849878a
sozemego <piknypiec@gmail.com>
2019-11-12 22:26:31 +0100

    web-client - send JWT to clock service when retrieving clock

--- web-client/src/clock/useGameClock.ts
+++ web-client/src/clock/useGameClock.ts
@@ -6,18 +6,20 @@ import { useDispatch } from "react-redux";
 import { getCurrentGameDate } from "./business";
 import { GameClockHook, UseGameClockInitial } from "./index";
 import { CLOCK_SERVICE_CLOCK_URL } from "../config/urls";
+import { useGetUser } from "../auth/selectors";
+import { getAsJson } from "../rest/client";
 
 export function useGameClock({ interval }: UseGameClockInitial): GameClockHook {
   const clock = useGetGameClock();
   const dispatch = useDispatch();
+  const user = useGetUser();
 
   useEffect(() => {
-    if (!clock.multiplier) {
-      fetch(CLOCK_SERVICE_CLOCK_URL)
-        .then(result => result.json())
+    if (!clock.multiplier && user) {
+      getAsJson(CLOCK_SERVICE_CLOCK_URL)
         .then(json => dispatch({ type: CLOCK_FETCHED, clock: json }));
     }
-  }, [clock.multiplier, dispatch]);
+  }, [clock.multiplier, dispatch, user]);
 
   const { time } = useRealClock({ interval });
   const gameDate = getCurrentGameDate(clock);
