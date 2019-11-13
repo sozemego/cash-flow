69447eddbdb4e25264a63f2eec8a7e7ca358f687
sozemego <piknypiec@gmail.com>
2019-11-13 22:12:52 +0100

    truck-service - connection to /websocket is now secure

---
+++ truck-service/src/main/java/com/soze/truck/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.soze.truck;
+
+import com.soze.common.security.AuthService;
+import com.soze.common.security.JWTBasicAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@ComponentScan("com.soze.common.security")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	private final AuthService authService;
+
+	@Autowired
+	public SecurityConfiguration(AuthService authService) {
+		this.authService = authService;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+				.and()
+				.csrf()
+				.disable()
+				.authorizeRequests()
+				.antMatchers("/websocket")
+				.authenticated()
+				.anyRequest()
+				.permitAll()
+				.and()
+				.addFilter(new JWTBasicAuthenticationFilter(authenticationManager(), authService));
+	}
+}
