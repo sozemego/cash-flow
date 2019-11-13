138022163057784a5f478e139e7e8219b9608f63
sozemego <piknypiec@gmail.com>
2019-11-13 22:12:27 +0100

    api-gateway - removed own security classed and imported from common instead

--- api-gateway/src/main/java/com/soze/cashflow/apigateway/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/soze/cashflow/apigateway/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.soze.cashflow.apigateway;
 
+import com.soze.common.security.AuthService;
+import com.soze.common.security.JWTBasicAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
+@ComponentScan("com.soze.common.security")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	private final AuthService authService;
