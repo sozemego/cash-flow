1b202d580faacbb6e2261fc72392c3c23d451ed8
sozemego <piknypiec@gmail.com>
2019-11-13 22:13:07 +0100

    web-client - send JWT to truck-service when connecting to websocket

--- web-client/src/truck/useTruckSocket.ts
+++ web-client/src/truck/useTruckSocket.ts
@@ -3,14 +3,18 @@ import { useWebsocket } from "../websocket/hook";
 import { useDispatch } from "react-redux";
 import { truckInit } from "./actions";
 import { useCallback } from "react";
+import { useGetUser } from "../auth/selectors";
 
 export function useTruckSocket() {
   const dispatch = useDispatch();
+  const user = useGetUser();
+  const token = user ? user.token : '';
 
   const reset = useCallback(() => dispatch(truckInit()), [dispatch]);
 
+
   const { socket, readyState } = useWebsocket(
-    TRUCK_SERVICE_SOCKET_URL,
+    TRUCK_SERVICE_SOCKET_URL + `?token=${token}`,
     dispatch,
     reset
   );
