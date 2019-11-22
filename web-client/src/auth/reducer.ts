51c40398f82a01dff10b91c9443c2a9b17e64d31
sozemego <piknypiec@gmail.com>
2019-11-22 20:43:12 +0100

    web-client - save user data in localStorage (at least for now)

--- web-client/src/auth/reducer.ts
+++ web-client/src/auth/reducer.ts
@@ -1,10 +1,10 @@
-import { AuthAction, AuthState } from "./index";
+import { AuthAction, AuthState, IUser } from "./index";
 import produce from "immer";
 import { USER_LOGGED_IN, USER_LOGGED_OUT } from "./actions";
 import { setToken } from "../rest/client";
 
 const initialState: AuthState = {
-  user: null
+  user: getUser()
 };
 
 export const reducer = produce((state = initialState, action: AuthAction) => {
@@ -12,12 +12,32 @@ export const reducer = produce((state = initialState, action: AuthAction) => {
     case USER_LOGGED_IN:
       state.user = action.user;
       setToken(state.user.token);
+      saveUser(action.user);
       return state;
     case USER_LOGGED_OUT:
       state.user = null;
       setToken(null);
+      saveUser(null);
       return state;
     default:
       return state;
   }
 });
+
+function getUser(): IUser | null {
+  const userItem = localStorage.getItem("user");
+  if (userItem === null) {
+    setToken(null);
+    return null;
+  }
+  const user = JSON.parse(userItem);
+  setToken(user.token);
+  return user;
+}
+
+function saveUser(user: IUser | null) {
+  if (user === null) {
+    localStorage.removeItem("user");
+  }
+  localStorage.setItem("user", JSON.stringify(user));
+}
