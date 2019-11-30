724bacfe1df6bc657c0a308dcb5cc7fd6cbf3367
sozemego <piknypiec@gmail.com>
2019-11-30 18:42:26 +0100

    log-aggregator-service - event logs are saved as they come

--- log-aggregator-service/src/main/java/com/soze/cashflow/logaggregator/service/LogAggregatorService.java
+++ log-aggregator-service/src/main/java/com/soze/cashflow/logaggregator/service/LogAggregatorService.java
@@ -10,23 +10,15 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Singleton
 public class LogAggregatorService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(LogAggregatorService.class);
 
-	private int flushEverySeconds = 2;
-	private LocalDateTime lastFlushTime = LocalDateTime.now();
-
 	private final LogRepository logRepository;
-	private final Queue<LogEventEntity> buffer = new ConcurrentLinkedQueue<>();
 
 	@Inject
 	public LogAggregatorService(LogRepository logRepository) {
@@ -36,16 +28,7 @@ public class LogAggregatorService {
 	public void handle(LogEventDTO logEventDTO) {
 		LOG.info("{}", logEventDTO);
 		LogEventEntity entity = convert(logEventDTO);
-		buffer.add(entity);
-
-		LocalDateTime now = LocalDateTime.now();
-
-		if (buffer.size() > 100 || now.minusSeconds(flushEverySeconds).isAfter(lastFlushTime)) {
-			LOG.info("Buffer size exceeded or time since last flush, saving LogEventEntities in a batch");
-			List<LogEventEntity> batch = sliceForBatchUpdate();
-			logRepository.save(batch);
-			lastFlushTime = LocalDateTime.now();
-		}
+		logRepository.save(entity);
 	}
 
 	private LogEventEntity convert(LogEventDTO dto) {
@@ -56,19 +39,6 @@ public class LogAggregatorService {
 		entity.setMessage(dto.message);
 		entity.setTimestamp(new Timestamp(Instant.now().toEpochMilli()));
 		return entity;
-	}
-
-	private List<LogEventEntity> sliceForBatchUpdate() {
-		List<LogEventEntity> batch = new ArrayList<>();
-
-		LogEventEntity current = null;
-		int count = 0;
-		while ((current = buffer.poll()) != null && count < 100) {
-			count++;
-			batch.add(current);
-		}
-
-		return batch;
 	}
 
 	public List<LogEventDTO> getAllLogs() {
